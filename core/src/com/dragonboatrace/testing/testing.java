1d38e5576af1207781cdeeb86c5ad229cc8be7d1
Babar Khan <72559963+babarkhanuni@users.noreply.github.com>
2021-01-30 15:36:01 +0000

    started testing.java

--- core/src/com/dragonboatrace/testing/testing.java
+++ core/src/com/dragonboatrace/testing/testing.java
@@ -1,5 +1,20 @@
 package com.dragonboatrace.testing;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+//imports to use libgdx
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.dragonboatrace.tools.Hitbox;
+
 
 public class testing {
-    
+
+    //testing for enitity.java
+    //entityType.java
+    //
 }
